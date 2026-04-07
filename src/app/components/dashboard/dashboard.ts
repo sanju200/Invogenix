6ff8f9f1e73ba2bcf3b5a57db5fb3c68c3694fc2
Sanjivani Bhongade <70435997+sanju200@users.noreply.github.com>
2026-04-07 14:01:14 +0530

    fixed style for header

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,9 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { projectData } from '../../dummyData/projectData';
+
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
-export class Dashboard {}
+export class Dashboard {
+  faEdit = faEdit;
+  faTrash = faTrash;
+  projects: any = projectData;
+}
