d5da59e87c9ef62c04db507e8d14099db1384dc9
Sanjivani Bhongade <70435997+sanju200@users.noreply.github.com>
2026-04-08 10:19:52 +0530

    updated organization detailed screen for signature upload

--- src/app/components/organization-details/organization-details.ts
+++ src/app/components/organization-details/organization-details.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef, NgZone } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faPrint } from '@fortawesome/free-solid-svg-icons';
 import { ImageUpload } from '../common/image-upload/image-upload';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-organization-details',
@@ -11,17 +12,72 @@ import { ImageUpload } from '../common/image-upload/image-upload';
   styleUrl: './organization-details.scss',
 })
 export class OrganizationDetails {
-  faPrint = faPrint
-  file: File | null = null;
-  
+  faPrint = faPrint;
 
-  onFileSelected(event: any) {
-    this.file = event.target.files[0];
+  signatureFile: File | null = null;
+  signaturePreview: SafeUrl | null = null;
+
+  stampFile: File | null = null;
+  stampPreview: SafeUrl | null = null;
+
+  constructor(
+    private sanitizer: DomSanitizer,
+    private cdr: ChangeDetectorRef,
+    private zone: NgZone
+  ) { }
+
+  onSignatureSelected(event: any) {
+    const file = event.target.files[0];
+    if (file && file.type.match(/image\/*/)) {
+      this.signatureFile = file;
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.zone.run(() => {
+          this.signaturePreview = this.sanitizer.bypassSecurityTrustUrl(reader.result as string);
+          this.cdr.detectChanges();
+        });
+      };
+      reader.readAsDataURL(file);
+    } else if (file) {
+      alert('Please select a valid image file (PNG, JPG, or JPEG)');
+    }
+  }
+
+  onStampSelected(event: any) {
+    const file = event.target.files[0];
+    if (file && file.type.match(/image\/*/)) {
+      this.stampFile = file;
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.zone.run(() => {
+          this.stampPreview = this.sanitizer.bypassSecurityTrustUrl(reader.result as string);
+          this.cdr.detectChanges();
+        });
+      };
+      reader.readAsDataURL(file);
+    } else if (file) {
+      alert('Please select a valid image file (PNG, JPG, or JPEG)');
+    }
+  }
+
+  clearSignature() {
+    this.signatureFile = null;
+    this.signaturePreview = null;
+    this.cdr.detectChanges();
+  }
+
+  clearStamp() {
+    this.stampFile = null;
+    this.stampPreview = null;
+    this.cdr.detectChanges();
   }
 
   uploadFile() {
-    if (this.file) {
-      console.log('Uploading file:', this.file);
+    if (this.signatureFile || this.stampFile) {
+      console.log('Uploading files:', {
+        signature: this.signatureFile,
+        stamp: this.stampFile
+      });
     }
   }
 }
