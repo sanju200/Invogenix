f3b055944de0232c4694464834cdbbea91824d71
Sanjivani Bhongade <70435997+sanju200@users.noreply.github.com>
2026-04-08 12:12:16 +0530

    added invite user screen

--- src/app/components/invite-users/invite-users.ts
+++ src/app/components/invite-users/invite-users.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCopy, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { invitations } from '../../dummyData/invitations';
+import { Clipboard } from '@angular/cdk/clipboard';
+import { MatTooltip, MatTooltipModule } from '@angular/material/tooltip';
+
 
 @Component({
   selector: 'app-invite-users',
-  imports: [],
+  imports: [FontAwesomeModule, MatTooltipModule],
   templateUrl: './invite-users.html',
   styleUrl: './invite-users.scss',
 })
-export class InviteUsers {}
+export class InviteUsers {
+  invitations: any[] = invitations;
+  faTrash = faTrash;
+  faCopy = faCopy;
+  constructor(private clipboard: Clipboard) { }
+
+  copyText(link: string, tooltip: MatTooltip) {
+    this.clipboard.copy(link);
+    tooltip.disabled = false;
+    tooltip.show();
+    setTimeout(() => { tooltip.disabled = true; }, 1500);
+  }
+}
---
+++ src/app/dummyData/invitations.ts
@@ -0,0 +1,22 @@
+export const invitations = [
+    {
+        "id": "cmicqtc0l001vqh0153tg7d1c",
+        "email": "sanjivani.bhongade@kizora.com",
+        "token": "86088a513d49295065c74490639d72da3ae6dcc2fe9cb180",
+        "expiresAt": "2025-12-08T06:05:24.885Z",
+        "usedAt": "2025-11-24T06:05:56.741Z",
+        "createdAt": "2025-11-24T06:05:24.886Z",
+        "isAdmin": true,
+        "inviteLink": "https://fasttrackinvo-dev.kizora.tech/signup?token=86088a513d49295065c74490639d72da3ae6dcc2fe9cb180"
+    },
+    {
+        "id": "cmnplui2s0006lm01di7ybkgu",
+        "email": "amayara@example.com",
+        "token": "94c2c90cb4ef9081ef26dbdbf2020526a8fa28c728228b41",
+        "expiresAt": "2026-04-22T05:25:53.284Z",
+        "usedAt": null,
+        "createdAt": "2026-04-08T05:25:53.285Z",
+        "isAdmin": false,
+        "inviteLink": "https://fasttrackinvo-dev.kizora.tech/signup?token=94c2c90cb4ef9081ef26dbdbf2020526a8fa28c728228b41"
+    },
+]
