f156e2acbc13a5f95f46fab9ff84c8f5900c9429
Sanjivani Bhongade <70435997+sanju200@users.noreply.github.com>
2026-04-08 18:01:26 +0530

    updated upload-excel modal

--- src/app/components/upload-excel/upload-excel.ts
+++ src/app/components/upload-excel/upload-excel.ts
@@ -6,6 +6,7 @@ import { CommonModule } from '@angular/common';
 import { faDownload, faFile, faUpload } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CdkDrag } from '@angular/cdk/drag-drop';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-upload-excel',
@@ -21,9 +22,52 @@ export class UploadExcel {
   faUpload = faUpload;
   isUploaded: boolean = false;
 
+
+
+  excelData: any[] = []; // 👈 store parsed data
+
   toggleContent() {
     this.isCustom = !this.isCustom;
   }
 
+  onFileChange(event: any) {
+    const file = event.target.files[0];
+
+    if (!file) return;
+
+    if (!file.name.endsWith('.xlsx') && !file.name.endsWith('.xls')) {
+      alert('Only Excel files allowed');
+      return;
+    }
+
+    const reader: FileReader = new FileReader();
+
+    reader.onload = (e: any) => {
+      const binaryStr = e.target.result;
+
+      const workbook = XLSX.read(binaryStr, { type: 'binary' });
+
+      const sheetName = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[sheetName];
+
+      this.excelData = XLSX.utils.sheet_to_json(worksheet, {
+        defval: ''
+      });
+
+      this.isUploaded = true;
+    };
+
+    reader.readAsBinaryString(file);
+
+  }
+
+  onFileDrop(event: DragEvent) {
+    event.preventDefault();
+    const file = event.dataTransfer?.files[0];
+
+    if (file) {
+      this.onFileChange({ target: { files: [file] } });
+    }
+  }
 
 }
