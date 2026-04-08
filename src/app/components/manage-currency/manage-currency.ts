cbc3a44937ff1259e82790326d60b8f3918a6eb0
Sanjivani Bhongade <70435997+sanju200@users.noreply.github.com>
2026-04-08 12:50:45 +0530

    added manage currency account screen

--- src/app/components/manage-currency/manage-currency.ts
+++ src/app/components/manage-currency/manage-currency.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCopy, faEdit, faEye, faEyeSlash, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { currencyData, currencyList } from '../../dummyData/currency';
 
 @Component({
   selector: 'app-manage-currency',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './manage-currency.html',
   styleUrl: './manage-currency.scss',
 })
-export class ManageCurrency {}
+export class ManageCurrency {
+  faCopy = faCopy;
+  faEye = faEye;
+  faEyeSlash = faEyeSlash;
+  faTrash = faTrash;
+  faEdit = faEdit;
+  currencyData: any = currencyList;
+  accounts: any[] = currencyData;
+  visibleAccounts: Record<string, boolean> = {};
+
+  showAccountNum(account: any) {
+    this.visibleAccounts[account.id] = !this.visibleAccounts[account.id];
+  }
+}
