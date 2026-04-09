efde7c02b0a7aa3c337a2e6314da9cc42e2465cf
Sanjivani Bhongade <70435997+sanju200@users.noreply.github.com>
2026-04-09 10:57:50 +0530

    added action on back button click

--- src/app/components/create-project/create-project.ts
+++ src/app/components/create-project/create-project.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
 import { currencyList } from '../../dummyData/currency';
+import { Location } from '@angular/common';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-create-project',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './create-project.html',
   styleUrl: './create-project.scss',
 })
 export class CreateProject {
+  faArrowLeft = faArrowLeft;
   TaxIdTypeLabel: string = 'VAT Number';
   agreementTypeLabel: string = 'SOW Reference';
   currencyData: any[] = currencyList;
+
+  constructor(public location: Location) { }
 
   onSelectChange(event: any) {
     const value = event.target.value;
@@ -20,5 +26,9 @@ export class CreateProject {
   onAgreementChange(event: any) {
     const value = event.target.value;
     this.agreementTypeLabel = value === 'SOW' ? 'SOW Reference' : 'MSA Reference';
+  }
+
+  onBackClick() {
+    this.location.back();
   }
 }
